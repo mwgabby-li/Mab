108e60de081ac7cee6ffc65a98b9c6563c89d0fc
Mark W. Gabby-Li <mark@gabby.li>
2023-04-10 10:59:12 -0700

    Moved Tokens from Symbols to Separate File
    Break circular dependency, and clearer separation of concepts.

---
+++ tokens/init.lua
@@ -0,0 +1,29 @@
+local lpeg = require 'lpeg'
+local P, C = lpeg.P, lpeg.C
+local l = require 'literals'
+local endToken = require('common').endToken
+
+local module = { op = {}, delim = {}, sep = {}, keyword = {}  }
+
+-- Delimiters
+module.delim.openFactor = l.delim.openFactor * endToken
+module.delim.closeFactor = l.delim.closeFactor * endToken
+module.delim.openBlock = l.delim.openBlock * endToken
+module.delim.closeBlock = l.delim.closeBlock * endToken
+
+-- Separators
+module.sep.statement = l.sep.statement * endToken
+
+module.keyword.return_ = l.kw.return_ * endToken
+
+module.op.assign = l.op.assign * endToken
+module.op.sum = C(P(l.op.add) + l.op.subtract) * endToken
+module.op.term = C(P(l.op.multiply) + l.op.divide + l.op.modulus) * endToken
+module.op.exponent = C(l.op.exponent) * endToken
+module.op.comparison = (C(l.op.greaterOrEqual) + C(l.op.greater) +
+                        C(l.op.lessOrEqual) + C(l.op.less) +
+                        C(l.op.equal) + C(l.op.notEqual)) * endToken
+module.op.unarySign = C(P(l.op.add) + l.op.subtract) * endToken
+module.op.print = l.op.print * endToken
+
+return module
