4b00b0bf9a5546ee4f4732b43c8a45966a8be67c
Mark W. Gabby-Li <mark@gabby.li>
2023-04-23 18:27:59 -0700

    Changes up to Lesson 6 Exercise 3-Garbage Collection
    popStack is a function that nils out popped stack indices. This is to be friendlier to automatic memory management on the Lua side.

--- interpreter/init.lua
+++ interpreter/init.lua
@@ -19,6 +19,14 @@ local function traceTwoCodes(trace, code, pc)
   end
 end
 
+local function popStack(stack, top, amount)
+  for i = top + amount - 1, top, -1 do
+    stack[top] = nil
+    top = top - 1
+  end
+  return top
+end
+
 function module.run(code, trace)
   local stack = {}
   local memory = {}
@@ -38,51 +46,51 @@ function module.run(code, trace)
     elseif code[pc] == 'add' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] + stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'subtract' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] - stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'multiply' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] * stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'divide' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] / stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'modulus' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] % stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'exponent' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] ^ stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'greater' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] > stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'less' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] < stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'greaterOrEqual' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] >= stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'lessOrEqual' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] <= stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'equal' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] == stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'notEqual' then
       traceBinaryOp(trace, code[pc], stack, top)
       stack[top - 1] = stack[top - 1] ~= stack[top] and 1 or 0
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'negate' then
       traceUnaryOp(trace, code[pc], stack[top])
       stack[top] = -stack[top]
@@ -102,7 +110,7 @@ function module.run(code, trace)
       traceTwoCodes(trace, code, pc)
       pc = pc + 1
       memory[code[pc] ] = stack[top]
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'newArray' then
       -- The size of the new array is on the top of the stack
       local size = stack[top]
@@ -125,22 +133,25 @@ function module.run(code, trace)
       
       -- Set the array to this value
       array[index] = value
+
       -- Pop the three things
-      top = top - 3
-    elseif code[pc] == 'getArray' then
+       top = popStack(stack, top, 3)
+   elseif code[pc] == 'getArray' then
       -- The array we are getting is one element below 
       local array = stack[top - 1]
       -- The index we're getting from the array is at the top
       local index = stack[top - 0]
       -- We have consumed two things, but we're about to add one:
       -- so just decrement by one to simulate popping two and pushing one.
-      top = top - 1
+      top = popStack(stack, top, 1)
       
       if index > array.size or index < 1 then
         error('Out of range. Array is size ' .. array.size .. ' but indexed at ' .. index .. '.')
       end
       
       -- Set the top of the stack to the value of this index of the array.
+      -- This is also now the index of where the array we loaded from was located,
+      -- so it has the benefit of removing the reference to that array from the stack.
       stack[top] = array[index]
     elseif code[pc] == 'jumpIfZero' then
       traceTwoCodes(trace, code, pc)
@@ -148,7 +159,7 @@ function module.run(code, trace)
       if stack[top] == 0 then
         pc = pc + code[pc]
       end
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'jump' then
       traceTwoCodes(trace, code, pc)
       pc = pc + 1
@@ -159,7 +170,7 @@ function module.run(code, trace)
       if stack[top] == 0 then
         pc = pc + code[pc]
       else
-        top = top - 1
+        top = popStack(stack, top, 1)
       end
     elseif code[pc] == 'jumpIfNonzeroJumpNoPop' then
       traceTwoCodes(trace, code, pc)
@@ -167,15 +178,18 @@ function module.run(code, trace)
       if stack[top] ~= 0 then
         pc = pc + code[pc]
       else
-        top = top - 1
+        top = popStack(stack, top, 1)
       end
     elseif code[pc] == 'print' then
       traceUnaryOp(trace, code[pc], stack[top])
       print(stack[top])
-      top = top - 1
+      top = popStack(stack, top, 1)
     elseif code[pc] == 'return' then
       traceUnaryOp(trace, code[pc], stack[top])
-      return stack[top]
+      -- Do it this way because return will probably not always exit the program...
+      local returnValue = stack[top]
+      top = popStack(stack, top, 1)
+      return returnValue
     else error 'unknown instruction'
     end
     pc = pc + 1
