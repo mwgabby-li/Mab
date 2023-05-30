mwgabby-li/Mab

commit e4260194caaa3f7e1d2f62f65846fd2c775121c2

Additional Elegance
Don't push anything when a function returns none. In a call, if the function is returning none, don't pop the return value because it wouldn't have pushed one.

Added some additional tests for this case and some earlier failure cases.

3 Translators/toStackVM.lua
@@ -45,9 +45,26 @@ function Translator:new(o)
   return o
 end
 
+function getTargetName(ast)
+  local target = ast.target
+  while target.tag == 'arrayElement' do
+    target = target.array
+  end
+  return target.name
+end
+
 function Translator:addError(...)
   if self.errorReporter then
     self.errorReporter:addError(...)
+  end
+end
+
+function Translator:getVariable(name)
+  local index, variable = self:findLocal(name)
+  if index then
+    return variable
+  else
+    return self.globals[name]
   end
 end
 
@@ -180,8 +197,7 @@ function Translator:codeExpression(ast)
     self:addCode('push')
     self:addCode(ast.value)
   elseif ast.tag == 'none' then
-    self:addCode('push')
-    self:addCode(false)
+    -- Don't add anything, this is nothing.
   elseif ast.tag == 'variable' then
     self:codeLoadVariable(ast)
   elseif ast.tag == 'functionCall' then
@@ -411,9 +427,14 @@ function Translator:codeStatement(ast)
     self:addCode((#self.locals - localsBeforeFunctionCodeStart) + #self.currentParameters)
   elseif ast.tag == 'functionCall' then
     self:codeFunctionCall(ast)
+
+    local name = getTargetName(ast)
+    local variable = self:getVariable(name)
     -- Discard return value for function statements, since it's not used by anything.
-    self:addCode('pop')
-    self:addCode(1)
+    if variable.type_.resultType.tag ~= 'none' then
+      self:addCode('pop')
+      self:addCode(1)
+    end
   elseif ast.tag == 'newVariable' then
     self:codeNewVariable(ast)
   elseif ast.tag == 'assignment' then
@@ -519,7 +540,6 @@ function Translator:codeFunction(ast)
   self.codingFunction = true
   self:codeStatement(ast.assignment)
   if self.currentCode[#self.currentCode - 1] ~= 'return' then
-    self:addCode('push')
     -- TODO: Doesn't support creating default returns for arrays.
     local resultTypeTag = ast.type_.resultType.tag
     if resultTypeTag == 'array' then
@@ -528,19 +548,21 @@ function Translator:codeFunction(ast)
     end
 
     if resultTypeTag == 'number' then
+      self:addCode('push')
       self:addCode(0)
     elseif resultTypeTag == 'boolean' then
+      self:addCode('push')
       self:addCode(false)
     elseif resultTypeTag == 'string' then
+      self:addCode('push')
       self:addCode ''
-      -- TODO: Rename 'unknown' to 'unspecified.'
     elseif resultTypeTag == 'none' then
       -- This is valid. Note that a function like this, with no return type,
       -- is only allowed to be executed as a statement.
       -- Any other use will cause a type checker error.
-      self:addCode(false)
     else
       self:addError('Internal error: unknown type "'..resultTypeTag..'" when generating automatic return value.')
+      self:addCode('push')
       self:addCode(0)
     end
     self:addCode('return')

mab.lua
@@ -58,7 +58,7 @@ local phases = {
     -- for the program to output, then prints
     -- the completion message.
     separatedOutput = true,
-    version = 3526977121,
+    version = 2345550064,
   },
 }
 

tests.lua
@@ -203,7 +203,7 @@ function module:testKeywordExcludeRules()
   lu.assertEquals(result, false)
   errorReporter, result = module.parse(wrapWithEntrypoint'return <- 1')
   lu.assertEquals(result, false)
-  errorReporter, result = module.parse(wrapWithEntrypoint'return return')
+  errorReporter, result = module.parse(wrapWithEntrypoint'call return()')
   lu.assertEquals(result, false)
 
   lu.assertEquals(self:fullTest('delta x: = 1   return delta x', true), 1)
@@ -1481,6 +1481,45 @@ entry point: -> number {
 }
 ]]
   lu.assertEquals(self:fullTest(input), 55)
+  
+  input =
+[[entry point: -> number {
+  lf: (n:number) -> number { return n }
+
+  return 12
+}]]
+
+  lu.assertEquals(self:fullTest(input), 12)
+
+  -- Function returning nothing
+  input =
+[[test:(a:number, b:number) -> {
+  local variable: 10
+  return
+}
+
+entry point: -> number {
+  call test(7, 12)
+  return 12
+}]]
+  lu.assertEquals(self:fullTest(input), 12)
+
+  -- Function returning nothing,
+  -- and local function defined but not called.
+  input =
+[[test:(a:number, b:number) -> {
+  local variable: 10
+  return
+}
+
+entry point: -> number {
+  lf: (n:number) -> number { return n }
+
+  call test(7, 12)
+  return 12
+}]]
+
+  lu.assertEquals(self:fullTest(input), 12)
 
 end
 

commit af1ffa97acc06501942de42b5035b2ed7c0284d4

Fix Infinite Loop in Input Program
The stack was being put into an inconsistent state because the interpreter was preserving the top element of the stack as if it was a return value, even though the function returned nothing so there was actually nothing there.
The end result is that it was wiping out what used to be on the top of the stack.

Added a new 'exit' instruction that is coded for functions that have no return values.

The bug was introduced in e4260194caaa3f7e1d2f62f65846fd2c775121c2, "Additional Elegance," as it added support for functions with no return values to not push values onto the stack.

3 Translators/toStackVM.lua
@@ -420,8 +420,18 @@ function Translator:codeStatement(ast)
     self:codeStatement(ast.firstChild)
     self:codeStatement(ast.secondChild)
   elseif ast.tag == 'return' then
+    -- If this function has no return values,
+    -- we need to code an 'exit' instead of a 'return,'
+    -- so the interpreter doesn't try to preserve a return
+    -- value that doesn't exist and put the stack in an
+    -- inconsistent state.
+    local numInstructions = #self.currentCode
     self:codeExpression(ast.sentence)
-    self:addCode('return')
+    if #self.currentCode > numInstructions then
+      self:addCode 'return'
+    else
+      self:addCode 'exit'
+    end
     -- Add the number of locals at this time so we can update the stack.
     local localsBeforeFunctionCodeStart = self.blockBases[self.functionBlockBase] - 1
     self:addCode((#self.locals - localsBeforeFunctionCodeStart) + #self.currentParameters)
@@ -539,33 +549,33 @@ function Translator:codeFunction(ast)
 
   self.codingFunction = true
   self:codeStatement(ast.assignment)
-  if self.currentCode[#self.currentCode - 1] ~= 'return' then
-    -- TODO: Doesn't support creating default returns for arrays.
+  -- If the function doesn't have a 'return' or 'exit,' we need to add one:
+  local penultimateInstruction = self.currentCode[#self.currentCode - 1]
+  if not (penultimateInstruction == 'return' or penultimateInstruction == 'exit') then
     local resultTypeTag = ast.type_.resultType.tag
-    if resultTypeTag == 'array' then
-      self:addError('TODO: Returning default array type not supported, add an explicit return to: "' ..
-                    ast.name or 'anonymous function' .. '."', ast.assignment)
-    end
-
-    if resultTypeTag == 'number' then
-      self:addCode('push')
-      self:addCode(0)
-    elseif resultTypeTag == 'boolean' then
-      self:addCode('push')
-      self:addCode(false)
-    elseif resultTypeTag == 'string' then
-      self:addCode('push')
-      self:addCode ''
-    elseif resultTypeTag == 'none' then
-      -- This is valid. Note that a function like this, with no return type,
-      -- is only allowed to be executed as a statement.
-      -- Any other use will cause a type checker error.
+    -- Code 'exit' if we don't have a return value,
+    -- so the VM will know not to preserve the
+    -- top stack value when the function ends.
+    if resultTypeTag == 'none' then
+      self:addCode('exit')
     else
-      self:addError('Internal error: unknown type "'..resultTypeTag..'" when generating automatic return value.')
       self:addCode('push')
-      self:addCode(0)
+      -- TODO: Doesn't support creating default returns for arrays.
+      if resultTypeTag == 'array' then
+        self:addError('TODO: Returning default array type not supported, add an explicit return to: "' ..
+                ast.name or 'anonymous function' .. '."', ast.assignment)
+      elseif resultTypeTag == 'number' then
+        self:addCode(0)
+      elseif resultTypeTag == 'boolean' then
+        self:addCode(false)
+      elseif resultTypeTag == 'string' then
+        self:addCode ''
+      else
+        self:addError('Internal error: unknown type "'..resultTypeTag..'" when generating automatic return value.')
+        self:addCode(0)
+      end
+      self:addCode('return')
     end
-    self:addCode('return')
     self:addCode(self.functionLocalsToRemove + #self.currentParameters)
     self.functionLocalsToRemove = nil
   end

4 Interpreter/stackVM.lua
@@ -295,12 +295,18 @@ function StackVM:run(code)
       printValue(self.stack[self.top])
       io.write '\n'
       self:popStack(1)
-    elseif code[pc] == 'return' then
-      self:traceCustom('return' .. (code[pc] == 0 and '' or ', pop ' .. code[pc + 1]))
+    -- 'return' outputs something, 'exit' is for functions that don't:
+    elseif code[pc] == 'return' or code[pc] == 'exit' then
+      self:traceCustom(code[pc] .. (code[pc + 1] == 0 and '' or ', pop ' .. code[pc + 1]))
       pc = pc + 1
       local pop = code[pc]
-      for i=self.top - pop,self.top do
-        self.stack[i] = self.stack[i + pop]
+      -- The basic idea here is to put the return values at the new top of the stack.
+      -- This loop is forward-looking for returning multiple values.
+      -- Only do this if we are returning (rather than exiting without return values)
+      if code[pc - 1] == 'return' then
+        for i=self.top - pop,self.top do
+          self.stack[i] = self.stack[i + pop]
+        end
       end
       self:popStack(pop)
       self:traceStack(base)

mab.lua
@@ -58,7 +58,7 @@ local phases = {
     -- for the program to output, then prints
     -- the completion message.
     separatedOutput = true,
-    version = 2345550064,
+    version = 3021864919,
   },
 }
 